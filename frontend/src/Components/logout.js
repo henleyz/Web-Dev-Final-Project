aabb2cab1fcd845d53e316454d52a6454a1374f5
Henley <henley@berkeley.edu>
2022-11-28 17:48:09 -0800

    undoed curtis login causing millions of error

--- frontend/src/Components/logout.js
+++ frontend/src/Components/logout.js
@@ -1,19 +1,19 @@
-import { googleLogout } from "@react-oauth/google";
+// import { googleLogout } from "@react-oauth/google";
 
-const clientId = "673795091694-s12v6e4o1ibtj47ltqn9mpp6n4ikad4a.apps.googleusercontent.com"
+// const clientId = "673795091694-s12v6e4o1ibtj47ltqn9mpp6n4ikad4a.apps.googleusercontent.com"
 
-function logout() {
-    const onSuccess = () => {
-        console.log()
-    }
-    return(
-    <div id="signOutButton">
-        <googleLogout
-        clientId={clientId}
-        text="Logout"
-        onLogoutSuccess={onSuccess}
-        />
-    </div>
-    )
-}
-export default logout;
+// function logout() {
+//     const onSuccess = () => {
+//         console.log()
+//     }
+//     return(
+//     <div id="signOutButton">
+//         <googleLogout
+//         clientId={clientId}
+//         text="Logout"
+//         onLogoutSuccess={onSuccess}
+//         />
+//     </div>
+//     )
+// }
+// export default logout;
