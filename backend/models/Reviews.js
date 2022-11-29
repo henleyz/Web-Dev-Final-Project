a4d0a670ff0a02c59d28205f5f884bbd9548dcd9
Curtis Yu <curtis_yu@berkeley.edu>
2022-11-28 23:00:39 -0800

    post review backend is working

--- backend/models/Reviews.js
+++ backend/models/Reviews.js
@@ -1,12 +1,17 @@
 const mongoose = require("mongoose");
 
 const ReviewSchema = mongoose.Schema({
-    username: {
+    title: {
         type: String,
+        required: true
     },
-    content: {
+    body: {
         type: String,
         required: true // Have to be exist when creating new user.
+    },
+    rate: {
+        type: Number,
+        required: true
     },
     createdAt: {
         type: Date,
--- backend/routes/review.js
+++ backend/routes/review.js
@@ -1,7 +1,25 @@
-const express = require("express");
 const Review = require("../models/Reviews");
+const express = require("express");
 const router = express.Router();
 
-router.get("/", () => console.log("BOOBOOO"))
+router.get("/", (req, res) => console.log("Console"))
+router.post("/post", async (req, res) => {
+    const {title, body, rate} = req.body;
+    try{
+        let review = new Review({
+            title: title,
+            body: body,
+            rate: rate
+        })
+        if (!review) return res.status(400).json({
+            msg: "Review doesn't seem right",
+        });
+        review.save()
+        return res.send(JSON.stringify(review))
+    } catch(e) {
+        console.log(e);
+        res.status(500).send("Error in posting review");
+    }
+})
 
 module.exports = router
