0b0a1dd66bb55f2075f403c92f90f1723c4180a7
Curtis Yu <curtis_yu@berkeley.edu>
2022-11-30 19:34:30 -0800

    fix minor issue

--- backend/index.js
+++ backend/index.js
@@ -18,7 +18,7 @@ app.use("/review", review)
 app.use("/busyness", busyness)
 
 // Change to true when you don't want to restart the database
-if (true) {
+if (false) {
     const clear = require("./clearDB");
     app.use("/clearDB", clear)
 }
