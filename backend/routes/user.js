e5c2661751bcd374738629988b3f3966c69c5ad8
Jacob Taegon Kim <jacobkim@berkeley.edu>
2022-12-02 00:28:16 -0800

    login fixed

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -54,9 +54,9 @@ router.post("/signup", async (req, res) => {
 router.post("/login", async (req, res) => {
     const {email, password} = req.body;
     try {
-        if (email =="" || password == "") {
-            return res.status(400).json({msg: "Empty String"})
-        }
+        // if (email =="" || password == "") {
+        //     return res.status(400).json({msg: "Empty String"})
+        // }
         let user = await User.findOne({email});
         if (!user) return res.status(400).json({msg: "User not Exist"});
 
